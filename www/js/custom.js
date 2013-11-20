065cc706be430509a7d95eafe8d4ddfce49cecac
jephou <jephou@3e-next.rw>
2013-11-21 01:37:37 +0800

    good

---
+++ www/js/custom.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function(){
+
+
+$(".td").click(function(){
+	
+	var id = $(this).siblings(".active").attr("id");
+	
+	if($(this).hasClass("active")){
+		return false;
+		}
+	$("#"+id).removeClass("active");
+	$(this).addClass("active");
+	});
+	
+});
